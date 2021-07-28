0e367de67df757f015f6338cad99ee304bac2c94
battle <root1@battledeMacBook-Pro.local>
2021-07-28 13:57:33 +0800

    Updates

--- component/Icon/index.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-declare const Icon: React.SFC<import('@ant-design/icons/lib/components/IconFont').IconFontProps>;
-export default Icon;
--- component/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { default as AdvancedSearchForm } from './AdvancedSearchForm';
-export { default as Icon } from './Icon';
-export { default as MultipleSelectList } from './MultipleSelectList';
-export { default as Input } from './Input';
-export { default as Table } from './Table';
-export { default as TablePro } from './Table/TablePro';
-export { default as AdvancedForm } from './AdvancedForm';
-export * from 'antd';
