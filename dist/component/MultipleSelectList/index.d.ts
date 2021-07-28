d41b6b40c9020708133c0df12558dd94d90f68ff
battle <root1@battledeMacBook-Pro.local>
2021-07-28 13:59:08 +0800

    fix

--- dist/component/MultipleSelectList/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { MultipleSelectListConfig } from '../../configs';
-interface IMultipleSelectListProps {
-    configs?: MultipleSelectListConfig[];
-    selectHandler?: any;
-}
-interface IMultipleSelectList extends React.FC<IMultipleSelectListProps> {}
-declare const MultipleSelectList: IMultipleSelectList;
-export default MultipleSelectList;
